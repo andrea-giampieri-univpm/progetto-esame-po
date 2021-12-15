24c5a1775cad9282360ce66c49e9765adb20e76c
Andrea Giampieri <s1093059@studenti.univpm.it>
2021-12-15 22:16:11 +0100

    interfaccia e classe esempio download dati

---
+++ PotrebbePiovere/src/main/java/com/pp/interfaces/InterfacePolling.java
@@ -0,0 +1,10 @@
+package com.pp.interfaces;
+
+public interface InterfacePolling {
+
+	/**
+	 * Definizione di un metodo per il download dei dati da owm
+	 */
+	public void getCurrentWeather();
+
+}
---
+++ PotrebbePiovere/src/main/java/com/pp/scheduled/DataPolling.java
@@ -0,0 +1,10 @@
+package com.pp.scheduled;
+
+/**
+ * Gestione delle attività schedulate di salvataggio dati
+ * @author Andrea Giampieri
+ *
+ */
+public class DataPolling {
+
+}
