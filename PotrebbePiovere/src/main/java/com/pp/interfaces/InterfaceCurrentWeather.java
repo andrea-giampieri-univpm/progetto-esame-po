89a1224fe40441799c60303e9812cb1710962e9a
Andrea Giampieri <s1093059@studenti.univpm.it>
2021-12-15 22:48:10 +0100

    aggiunta interfaccia currentweather

---
+++ PotrebbePiovere/src/main/java/com/pp/interfaces/InterfaceCurrentWeather.java
@@ -0,0 +1,22 @@
+package com.pp.interfaces;
+
+/**
+ * Interfacce per l'implementazione della classe
+ * CurrentWeather specifica per il progetto
+ * @author jump
+ *
+ */
+public interface InterfaceCurrentWeather {
+	
+	/**
+	 * aggiunge una riga di json contenente il meteo ad un file di testo
+	 */
+	public void appendToFile();
+	
+	/**
+	 * ritorna stringa json ottenuta con jsonsimple del modello semplificato specifico per l'applicazione
+	 */
+	public String toJsonString();
+
+
+}
