1973ccab3bf734b0bb2a35ea5d55ad76bf39feca
Andrea Giampieri <s1093059@studenti.univpm.it>
2021-12-15 23:30:08 +0100

    implementato servizio schedulato per dati meteo

--- PotrebbePiovere/src/main/java/com/pp/scheduled/DataPolling.java
+++ PotrebbePiovere/src/main/java/com/pp/scheduled/DataPolling.java
@@ -1,10 +1,34 @@
 package com.pp.scheduled;
 
+import java.util.ArrayList;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import com.pp.interfaces.InterfacePolling;
+import com.pp.model.CurrentWeather;
+import com.pp.utils.Config;
+
 /**
- * Gestione delle attività schedulate di salvataggio dati
+ * Gestione delle attività schedulate di salvataggio dati con spring
  * @author Andrea Giampieri
  *
  */
-public class DataPolling {
-
+@Component
+public class DataPolling implements InterfacePolling{
+	
+	
+	@Scheduled(fixedRate = 10000) //inserire da config
+	@Async
+	public void getCurrentWeather() {
+		RestTemplate restTemplate = new RestTemplate(); //oggetto mapper
+		ArrayList<Long> cities = Config.getCities(); //lista delle citta da interrogare
+		for(Long city: cities) {
+			CurrentWeather cw = restTemplate.getForObject("https://api.openweathermap.org/data/2.5/weather?id="+city.longValue()+"&appid="+Config.getConf("owm_apikey")+"&units=metric&lang=it", CurrentWeather.class);
+			System.out.println(cw); //output per diagnostica
+			cw.appendToFile(); //salvo il file
+		}
+	}
 }
