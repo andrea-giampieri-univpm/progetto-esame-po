294d7f619e88edb910c04307b2a388149d772df4
Andrea Giampieri <s1093059@studenti.univpm.it>
2021-12-20 00:32:52 +0100

    aggiunto getinstant per array in post

--- PotrebbePiovere/src/main/java/com/pp/controller/CurrentWeatherController.java
+++ PotrebbePiovere/src/main/java/com/pp/controller/CurrentWeatherController.java
@@ -1,6 +1,11 @@
 package com.pp.controller;
 
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +20,7 @@ import com.pp.model.CurrentWeather;
 @RestController
 public class CurrentWeatherController {
 
-	@GetMapping("/getinstant")
+	@GetMapping(value="/getinstant",produces = "application/json;")
 	public String getInstant(@RequestParam(value = "cityid") long cityId) {
 		String jsonstring; //stringa ritorno
 		try {
@@ -28,5 +33,32 @@ public class CurrentWeatherController {
 		}
 		return jsonstring;
 	}
+	
+	@SuppressWarnings("unchecked")
+	@PostMapping(value="/getinstantarr",produces = "application/json;")
+	public String getInstant(@RequestBody String cities) {
+		String jsonstring="";
+		JSONArray retArr = new JSONArray();
+		
+		try {
+			JSONArray cityArr = (JSONArray) new JSONParser().parse(cities);
+			
+			for(Object city: cityArr) {
+				CurrentWeather cw = new CurrentWeather(Long.parseLong(city.toString()));
+				retArr.add(new JSONParser().parse(cw.toJsonString()));
+			}
+			jsonstring=retArr.toString();
+			jsonstring=retArr.toJSONString();
+		} catch (CurrentWeatherException e) {
+			jsonstring=e.toString();
+		}  catch (ParseException e) {
+			jsonstring=e.toString();
+		} finally {
+			System.out.println("ricevuta chiamata api getinstant (demo del blocco finally)");
+		}
+		
+		return jsonstring;
+	}
+	
 }
 
